4c63d8857d31c28b810bf1d7a830f1047e2f36ce
MariamRazmadze <mariam.razmadze.1@iliauni.edu.ge>
2023-05-29 00:10:36 +0400

    refactor navbar component, move state to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import SearchComponent from "./components/SearchComponent";
+import { useState } from "react";
 
 function App() {
+  const [isMoon, setIsMoon] = useState(true);
+  const handleClick = () => {
+    setIsMoon(!isMoon);
+    document.body.style.backgroundColor = isMoon ? "#f6f8ff" : "#141D2F";
+  };
   return (
     <div>
-      <Navbar />
+      <Navbar isMoon={isMoon} handleClick={handleClick} />
       <SearchComponent />
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,42 +1,39 @@
 import classes from "./Navbar.module.css";
-import { useState } from "react";
 import iconMoon from "../assets/icon-moon.svg";
 import iconSun from "../assets/icon-sun.svg";
 
 const moon = iconMoon;
 const sun = iconSun;
 
-function Navbar() {
-  const [isMoon, setIsMoon] = useState(true);
+interface NavbarProps {
+  isMoon: boolean;
+  handleClick: () => void;
+}
 
-  const handleClick = () => {
-    setIsMoon(!isMoon);
-    document.body.style.backgroundColor = isMoon ? "#141D2F" : "#f6f8ff";
-    const title = document.getElementById("title");
-    if (title) {
-      title.classList.toggle(classes.darkMode);
-    }
-    const modeText = document.getElementById("modeText");
-    if (modeText) {
-      modeText.classList.toggle(classes.darkMode);
-    }
-  };
-
+function Navbar(props: NavbarProps) {
   return (
     <div>
       <div className={classes.navbar}>
-        <h1 id="title" className={classes.title}>
+        <h1
+          id="title"
+          className={`${classes.title} ${props.isMoon ? classes.darkMode : ""}`}
+        >
           devfinder
         </h1>
         <div className={classes.mode}>
-          <h2 id="modeText" className={classes.modeText}>
-            dark
+          <h2
+            id="modeText"
+            className={`${classes.modeText} ${
+              props.isMoon ? classes.darkMode : ""
+            }`}
+          >
+            {props.isMoon ? "light" : "dark"}
           </h2>
           <div>
             <img
-              src={isMoon ? moon : sun}
+              src={props.isMoon ? sun : moon}
               alt="modeIcon"
-              onClick={handleClick}
+              onClick={props.handleClick}
             />
           </div>
         </div>
